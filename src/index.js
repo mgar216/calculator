3cd682b2a7597123fa54935fea7a8685988ee1d8
Michael Garcia <mgar216@users.noreply.github.com>
2021-08-29 07:04:26 -0400

    Finished up Javascript logic for the calculator -- Looks good, just need to add a clear button at the bottom.

---
+++ src/index.js
@@ -0,0 +1,131 @@
+function add(a, b){
+    return Number(a) + Number(b);
+}
+function substract (a, b){
+    return Number(a) - Number(b);
+}
+function multiply(a, b){
+    return Number(a) * Number(b);
+}
+function divide(a, b){
+    return Number(a) / Number(b);
+}
+
+function updateDisplay(val){
+    let display = document.getElementById('calcCurrent')
+    if (String(display.textContent).length < 10){
+        display.textContent = display.textContent + val
+    }
+}
+
+function clearDisplay(){
+    let display = document.getElementById('calcCurrent')
+    display.textContent = ''
+}
+
+function getDisplayValue(){
+    let display = document.getElementById('calcCurrent')
+    return Number(display.textContent)
+}
+
+function clearOpDisplay(){
+    let opDisplay = document.getElementById('opCurrent')
+    opDisplay.textContent = ''
+}
+
+function updateOpDispay(val){
+    let opDisplay = document.getElementById('opCurrent')
+    opDisplay.textContent = String(val)
+}
+
+function returnCurrent(val1, mod, val2){
+    val1 = Number(val1)
+    val2 = Number(val2)
+    let total = 0
+    if (mod == '%' && Number(val2) == 0) return 'Undefined'
+    if (mod == '+') total = val1 + val2;
+    else if (mod == '-') total = val1 - val2;
+    else if (mod == '*') total = val1 * val2;
+    else if (mod == '%') total = val1 / val2;
+    return total
+}
+
+function returnOpDisplay(val1, mod, val2, display){
+    return `${val1} ${mod} ${val2} = ${display}`
+}
+
+function clearAll(){
+    display.textContent = ''
+    opDisplay.textContent = ''
+    val1 = 0
+    val2 = 0
+    mod = ''
+    isPost = false
+}
+
+function setDecimal(val){
+    if (!String(val).includes('.')) return String(val) + '.';
+    else return val;
+}
+
+var display = document.getElementById('calcCurrent')
+var opDisplay = document.getElementById('opCurrent')
+var val1 = 0
+var val2 = 0
+var mod = ''
+var isPost = false
+
+const dataKeys = document.querySelectorAll('.data-key')
+// Set up data-keys to return their value on click
+dataKeys.forEach((key) => {
+    key.addEventListener('click', () => {
+        if (isPost) clearAll();
+        updateDisplay(key.textContent)
+    })
+})
+
+const specialKeys = document.querySelectorAll('.specialKeys')
+// Set up special keys to perform an action (add, subtract, equal, etc.)
+specialKeys.forEach((key) => {
+    key.addEventListener('click', () => {
+        if (mod == ''){
+            val1 = Number(display.textContent)
+            mod = key.textContent
+            display.textContent = ''
+            opDisplay.textContent = String(val1) + ' ' + mod
+        }
+    })
+})
+
+// Equal sign special functionality
+const equal = document.getElementById('equal')
+equal.addEventListener('click', () => {
+    if (mod && display.textContent){
+        val2 = Number(display.textContent)
+        let newDisplay = String(returnCurrent(val1, mod, val2))
+        if (newDisplay.length > 10){
+            newDisplay = newDisplay.slice(0, 9)
+        }
+        display.textContent = newDisplay
+        opDisplay.textContent = returnOpDisplay(val1, mod, val2, display.textContent)
+        console.log(display)
+        val1 = 0
+        val2  = 0
+        mod = ''
+        isPost = true
+    }
+})
+
+const decimal = document.getElementById('decimal')
+decimal.addEventListener('click', () => {
+    display.textContent = setDecimal(display.textContent)
+})
+
+const sign = document.getElementById('sign')
+sign.addEventListener('click', () => {
+    if (display.textContent && !isPost){
+        display.textContent = Number(display.textContent) * -1
+    } else {
+        display.textContent = '-'
+    }
+})
